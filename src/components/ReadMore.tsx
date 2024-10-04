30fe5c331d3f78b0e4eb27cbc2c33b209d3df9e9
vtancara <veronica.tancara.c@gmail.com>
2024-10-03 23:53:45 -0400

    fix: ajustando seccion de iconos de informacion

--- src/components/ReadMore.tsx
+++ src/components/ReadMore.tsx
@@ -1,29 +1,25 @@
 import React from "react";
 import { Container } from "@/components/Container";
+import Image from "next/image";
+import { ReadMoreElement } from "./ReadMoreElement";
 
 export const ReadMore = () => {
   return (
-    <Container>
-      <div className="relative items-center justify-between w-full max-w-4xl gap-5 mx-auto text-white bg-indigo-600 px-7 py-7 lg:px-12 lg:py-12 lg:flex-nowrap -mt-32">
-        <div className="flex-grow text-center lg:text-left">
-          <h2 className="text-2xl font-medium lg:text-3xl">
-            Ready to try-out this template?
-          </h2>
-          <p className="mt-2 font-medium text-white text-opacity-90 lg:text-xl">
-            Don&apos;t let your visitors see a poor landing.
-          </p>
-        </div>
-        <div className="flex-shrink-0 w-full text-center lg:w-auto">
-          <a
-            href="https://github.com/web3templates"
-            target="_blank"
-            rel="noopener"
-            className="inline-block py-3 mx-auto text-lg font-medium text-center text-indigo-600 bg-white rounded-md px-7 lg:px-10 lg:py-5 "
-          >
-            Download for Free
-          </a>
+      <div className="relative items-center justify-between w-full max-w-5xl gap-5 mx-auto text-gray-800 bg-white px-7 py-7 lg:px-12 lg:py-6 lg:flex-nowrap -mt-32">
+      <div className="flex lg:flex-row md:flex-row flex-col items-center text-center">
+          <div className="basis-1/4">
+            <ReadMoreElement image={"/img/read-more/001-school.png"} text={"Unidad de postgrado"}/>
+          </div>
+          <div className="basis-1/4">
+            <ReadMoreElement image={"/img/read-more/002-virtual-assistant.png"} text={"Campus virtual"} />
+          </div>
+          <div className="basis-1/4">
+            <ReadMoreElement image={"/img/read-more/003-website.png"} text={"Informaciones"} />
+          </div>
+          <div className="basis-1/4">
+            <ReadMoreElement image={"/img/read-more/004-social-media.png"} text={"Redes sociales"} />
+          </div>
         </div>
       </div>
-    </Container>
   );
 };
---
+++ src/components/ReadMoreElement.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Container } from "@/components/Container";
+import Image from "next/image";
+
+interface ReadMoreElementType {
+  image: string
+  text: string
+}
+
+export const ReadMoreElement = ({ image, text }: ReadMoreElementType) => {
+  return (
+    <>
+      <div className="container p-6 ">
+        <div className="flex justify-center px-4">
+          <Image
+            src={image}
+            height={'200'}
+            width={'200'}
+            alt={''}
+          />
+        </div>
+        <div className="flex-grow p-2">
+          <p className="mt-2 font-medium text-opacity-90 lg:text-xl">
+            {text}
+          </p>
+        </div>
+        <div className="flex-shrink-0 w-full text-center lg:w-auto">
+          <a
+            href=""
+            target="_blank"
+            rel="noopener"
+            className="text-customBlueBtnLink text-lg font-medium text-center"
+          >
+            Ver más
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
