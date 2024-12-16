a9ca6c469a517283b6f88d6e4a63f9586e51bd5c
Ion Villarreal <ivillarreal@agetic.gob.bo>
2024-12-16 19:40:08 -0400

    fix: corrección de tipado para Slider y detalle de docentes

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,75 +1,72 @@
-import { useEffect, useState } from "react";
-import EventCard from "./EventCard";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import {ReactNode, useEffect, useState} from "react";
+import {ChevronLeft, ChevronRight} from "lucide-react";
 import useBreakpoints from "@/hooks/useBreakpoint";
 
 
-const Slider = ({
-  items
-}) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const [isAnimating, setIsAnimating] = useState(false);
-  const { isXs, isSm, isLg } = useBreakpoints();
+const Slider = ({items}: { items: Array<ReactNode> }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [isAnimating, setIsAnimating] = useState(false);
+    const {isXs, isSm, isLg} = useBreakpoints();
 
-  let elementsInSlide = 3
-  if (isSm && !isLg) elementsInSlide = 2
-  if (isXs && !isSm) elementsInSlide = 1
+    let elementsInSlide = 3
+    if (isSm && !isLg) elementsInSlide = 2
+    if (isXs && !isSm) elementsInSlide = 1
 
-  const nextSlide = () => {
-    if (!isAnimating) {
-      setIsAnimating(true);
-      setCurrentIndex((prevIndex) => {
-        return prevIndex === items.length - elementsInSlide ? 0 : prevIndex + 1
-      }
-      );
-    }
-  };
+    const nextSlide = () => {
+        if (!isAnimating) {
+            setIsAnimating(true);
+            setCurrentIndex((prevIndex) => {
+                    return prevIndex === items.length - elementsInSlide ? 0 : prevIndex + 1
+                }
+            );
+        }
+    };
 
-  const prevSlide = () => {
-    if (!isAnimating) {
-      setIsAnimating(true);
-      setCurrentIndex((prevIndex) =>
-        prevIndex === 0 ? items.length - elementsInSlide : prevIndex - 1
-      );
-    }
-  };
+    const prevSlide = () => {
+        if (!isAnimating) {
+            setIsAnimating(true);
+            setCurrentIndex((prevIndex) =>
+                prevIndex === 0 ? items.length - elementsInSlide : prevIndex - 1
+            );
+        }
+    };
 
-  useEffect(() => {
-    const timer = setTimeout(() => setIsAnimating(false), 300);
-    return () => clearTimeout(timer);
-  }, [currentIndex]);
+    useEffect(() => {
+        const timer = setTimeout(() => setIsAnimating(false), 300);
+        return () => clearTimeout(timer);
+    }, [currentIndex]);
 
 
-  return (
-    <div className="relative max-w-5xl mx-auto">
-      <div className="overflow-hidden">
-        <div
-          className="flex transition-transform duration-300 ease-in-out"
-          style={{ transform: `translateX(-${currentIndex * (100 / elementsInSlide)}%)` }}
-        >
-          {items.map((item, index) => (
-            <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 flex-shrink-0 px-2">
-              {item}
+    return (
+        <div className="relative max-w-5xl mx-auto">
+            <div className="overflow-hidden">
+                <div
+                    className="flex transition-transform duration-300 ease-in-out"
+                    style={{transform: `translateX(-${currentIndex * (100 / elementsInSlide)}%)`}}
+                >
+                    {items.map((item, index) => (
+                        <div key={index} className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 flex-shrink-0 px-2">
+                            {item}
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))}
+            <button
+                onClick={prevSlide}
+                className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/2 bg-white p-2 rounded-full shadow-md"
+                disabled={isAnimating}
+            >
+                <ChevronLeft className="w-6 h-6 text-gray-600"/>
+            </button>
+            <button
+                onClick={nextSlide}
+                className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 bg-white p-2 rounded-full shadow-md"
+                disabled={isAnimating}
+            >
+                <ChevronRight className="w-6 h-6 text-gray-600"/>
+            </button>
         </div>
-      </div>
-      <button
-        onClick={prevSlide}
-        className="absolute top-1/2 left-0 -translate-y-1/2 -translate-x-1/2 bg-white p-2 rounded-full shadow-md"
-        disabled={isAnimating}
-      >
-        <ChevronLeft className="w-6 h-6 text-gray-600" />
-      </button>
-      <button
-        onClick={nextSlide}
-        className="absolute top-1/2 right-0 -translate-y-1/2 translate-x-1/2 bg-white p-2 rounded-full shadow-md"
-        disabled={isAnimating}
-      >
-        <ChevronRight className="w-6 h-6 text-gray-600" />
-      </button>
-    </div>
-  );
+    );
 }
 
 export default Slider;
